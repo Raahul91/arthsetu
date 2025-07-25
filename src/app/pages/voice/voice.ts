916162516575757e3db0ad7bb84bc81fa921aa54
Yashashri <bhandekaryashashri10@gmail.com>
2025-07-25 11:11:08 +0530

    mic icon: done

--- src/app/pages/voice/voice.ts
+++ src/app/pages/voice/voice.ts
@@ -277,6 +277,7 @@ export class VoicePage {
   }
 
   async listen() {
+     this.isListening = true;
     console.log('listen action triggered');
     // if (this.isListening) {
     //   this.speech.stopListening();
@@ -306,6 +307,10 @@ export class VoicePage {
             (matches) => {
                console.log('Speech matches:', matches);
                this.stopListening();
+               setTimeout(() => {
+                  this.isListening = false;
+                  console.log('Stopped listening');
+                }, 5000);
               _this.zone.run(() => {
               _this.matches = matches;
             })
@@ -313,6 +318,7 @@ export class VoicePage {
             },
             (error) => {
                     console.error('Error during recognition:', error);
+                    this.isListening = false;
                     this.stopListening();     
             }
           );
